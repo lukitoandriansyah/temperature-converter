9b4d7de77ac313ce2476372fb552a2b19495432f
LUKITO\lukit <lukitoandriansyah45#@gmail.com>
2024-07-14 02:12:34 +0700

    Kelimax

--- temperatureConverter/src/main/java/id/clientless/tc/temperatureConverter/config/WebConfig.java
+++ temperatureConverter/src/main/java/id/clientless/tc/temperatureConverter/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("*") // Mengizinkan semua asal
+                .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
     }
 }
